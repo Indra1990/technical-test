4c631cc0b5bc7beeb0997d045935d55ed6d8c1a1
indra1990 <muhammad.indra65@gmail.com>
2022-07-26 15:51:06 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 1; i <= 9; i++ {
+		for j := 1; j < i+1; j++ {
+			fmt.Print(i, "*", j, "=", i*j)
+			fmt.Print("\t")
+		}
+		fmt.Println()
+	}
+
+}
